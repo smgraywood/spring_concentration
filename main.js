eea5c3299daea066a3cbbb9e45837f0d06eaefa7
Sarah Graywood <smgraywood@gmail.com>
2023-04-04 15:29:04 -0700

    add winning message logic

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const cardsContainerEl = document.querySelector("#gameBoard");
 const cards = document.querySelectorAll(".flip-card");
 const startButton = document.querySelector("#start-button");
 const cardNumberSelector = document.getElementById("select-cards-number");
+const statusMessage = document.querySelector("#status-message")
 
 /*----- state variables -----*/
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+let chosen
+let matches
 let time;
+
 
 /*----- cached elements  -----*/
 
@@ -34,6 +35,13 @@ cardNumberSelector.addEventListener("change", function () {
 
 /*----- functions -----*/
 
+
+init()
+
+function init(){
+	chosen = []
+	matches = 0
+}
 // function init(evt){
 //     evt.target.startTimer()
 // }
@@ -43,8 +51,31 @@ cardNumberSelector.addEventListener("change", function () {
 // }
 
 function flipped(evt) {
-	evt.target.closest(".flip-card-inner").style.transform = "rotateY(180deg)";
 
+	const card = evt.target.closest(".flip-card-inner")
+	card.style.transform = "rotateY(180deg)";
+
+	card.classList.toggle('flipped');
+
+	chosen.push(card.dataset.id)
+
+	console.log(matches)
+	console.log(chosen) 
+	if(chosen.length === 2){
+		matches = chosen.every(id => id === chosen[0]) ? matches + 1: matches + 0 
+		chosen = []
+	if(matches.length >= 1){
+		const card = evt.target.closest(".flip-card-inner")
+		card.style.transform = "rotateY(180deg)"	
+	}
+	if(matches === 12){
+		statusMessage.textContent = "You won!"
+	}
+	else{
+
+	}
+		render()
+	}
 	// firstCard = ""
 	// secondCard = ""
 	// if (firstCard === ""){
@@ -59,21 +90,9 @@ function flipped(evt) {
 	// }
 }
 
-function checkIfMatch() {
-	let flippedCards = document.querySelectorAll(".flip-card.flipped");
-	if (flippedCards.length === 2) {
-		let card1 = flippedCards[0].getAttribute("data-flowers");
-		let card2 = flippedCards[1].getAttribute("data-flowers");
-		if (card1 === card2) {
-			// cards match, keep them flipped over
-			flippedCards.forEach((card) => card.classList.add("matched"));
-		} else {
-			// cards don't match, flip them back over
-			flippedCards.forEach((card) => card.classList.remove("flipped"));
-		}
-	}
-}
+function render(){
 
+}
 
 // function checkIfMatch(){
 //     if (firstCard (need to access source image and compare) === secondCard){
