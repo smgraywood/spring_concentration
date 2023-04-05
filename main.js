710919cc45f0609f46b2ccef05cf92e519e1dcaa
Sarah Graywood <smgraywood@gmail.com>
2023-04-05 14:32:58 -0700

    add gif to html, css styling, js loss logic

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ const cards = document.querySelectorAll(".flip-card");
 const startButton = document.querySelector("#start-button");
 const cardNumberSelector = document.getElementById("select-cards-number");
 const statusMessage = document.querySelector("#status-message");
-const timeLeft = document.getElementById("timer")
-const timerDiv = document.getElementById("timer-div")
-
+const timeLeft = document.getElementById("timer");
+const timerDiv = document.getElementById("timer-div");
 
 /*----- state variables -----*/
 let chosen;
@@ -23,78 +22,106 @@ let card2;
 
 cardsContainerEl.addEventListener("click", flipCard);
 
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
+
+cardNumberSelector.addEventListener("change", function () {
+	if (this.getAttribute("name") === "count") {
+		const number = parseInt(this.value);
+		const gameBoardBoxes = document.querySelectorAll(".gameBoardBox");
+		for (let i = 0; i < gameBoardBoxes.length; i++) {
+			if (i < number) {
+				gameBoardBoxes[i].style.display = "block";
+			} else {
+				gameBoardBoxes[i].style.display = "none";
+				gameBoardBoxes[i].style.pointerEvents = "none";
+			}
+		}
+	}
+});
 
 /*----- functions -----*/
 
-
-function flipCard(evt){
-	const cardContainer = evt.target.closest('.flip-card-inner')
-	cardContainer.style.transform = 'rotateY(180deg)'
-	flipped(cardContainer)
+function flipCard(evt) {
+	const cardContainer = evt.target.closest(".flip-card-inner");
+	cardContainer.style.transform = "rotateY(180deg)";
+	flipped(cardContainer);
 }
-function startGame(){
+function startGame() {
+	gameBoard.style.pointerEvents = "auto";
+	cardNumberSelector.style.pointerEvents = "none";
+	startButton.style.pointerEvents = "none";
 	init();
-	timerDiv.style.display = 'flex'
+	timerDiv.style.display = "flex";
 	timer = setInterval(updateTimer, 1000);
-	updateTimer()
+	updateTimer();
+	//reset board
 }
 
 function init() {
 	chosen = [];
 	matches = 0;
-	chosen = []
-	timerLeft = 60
+	chosen = [];
+	timerLeft = 60;
 }
 
 function flipped(cardContainer) {
 	const card = cardContainer;
-	if (chosen.length === 0){
-		card1=card
+	if (chosen.length === 0) {
+		card1 = card;
+	} else if (chosen.length === 1) {
+		card2 = card;
 	}
-	else if(chosen.length === 1){
-		card2=card
-	}
-	chosen.push(card.dataset.id)
+	chosen.push(card.dataset.id);
 
 	if (chosen.length === 2) {
 		if (chosen[0] === chosen[1]) {
-			matches++
+			matches++;
 			chosen = [];
 		} else {
-			chosen = []
-			const cardsToFlip = [card1, card2]
+			chosen = [];
+			const cardsToFlip = [card1, card2];
 			setTimeout(() => {
 				cardsToFlip.forEach((card) => {
-					card.style.transform = 'rotateY(0deg)'
-				})
+					card.style.transform = "rotateY(0deg)";
+				});
 			}, 1000);
 		}
 	}
-
-	if (matches === 12 && time > 0) {
-		statusMessage.textContent = "You won!";
-		//enable start button
-	}
 	// if(matches === 0 && time === 0)
-	// 	statusMessage.textContent = "You lost."
+	//
 	// else{
 
 	// }
 	// render()
 }
 
-function gameOver(){
-	clearInterval(timer)
-	document.getElementById('start-button').style.display = 'block';
+function gameOver() {
+	clearInterval(timer);
+	document.getElementById("start-button").style.display = "block";
+
+	// if (matches === number of cards selected) {
+	// 	//stop timer
+	// 	statusMessage.textContent = "You won!";
+	// 	startButton.style.pointerEvents = "auto";
+
+	// }
+	// else{
+	statusMessage.textContent = "You lost.";
+	startButton.style.pointerEvents = "auto";
+	gameBoard.style.pointerEvents = "none";
+	cardNumberSelector.style.pointerEvents = "auto";
+	// }
+
+	//reset board
 }
 
-function updateTimer(){
-	timerLeft = timerLeft-1
-	if(timerLeft >=0)
-	document.getElementById('timer').innerHTML = `${timerLeft} seconds remaining`
-	else
-	gameOver()
+function updateTimer() {
+	timerLeft = timerLeft - 1;
+	if (timerLeft >= 0)
+		document.getElementById(
+			"timer"
+		).innerHTML = `${timerLeft} seconds remaining`;
+	else gameOver();
 }
 
 (function shuffle() {
@@ -104,5 +131,4 @@ function updateTimer(){
 	});
 })();
 
-//to do startTimer
-
+//to do stoptimer when game is won
