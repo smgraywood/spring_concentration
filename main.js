771316e016972231dad4700911e9fb5f2b721bd8
Sarah Graywood <smgraywood@gmail.com>
2023-04-06 08:15:42 -0700

    add line by line comments to js file for clarity of code

--- main.js
+++ main.js
@@ -65,38 +65,56 @@ function flipCard(evt) {
 //(i.e. reset the board, reenable the game board pointer events, disable the board size selector
 //disable the start button, initialize the necessary values, show the timer, and start the timer)
 function startGame() {
+	//resetting the board to its original state
 	resetBoard()
+	//enabling clicks on the game board
 	gameBoard.style.pointerEvents = "auto";
+	//disabling clicks on the board size selector dropdown
 	cardNumberSelector.style.pointerEvents = "none";
+	//disabling clicks on the start button during game play
 	startButton.style.pointerEvents = "none";
+	//reestablishing the initial values
 	init();
+	//displaying the timer
 	timerDiv.style.display = "flex";
+	//starting the timer countdown
 	timer = setInterval(updateTimer, 1000);
+	//updating the timer as game play happens
 	updateTimer();
 }
 
 //function to initialize the values for the chosen number of cards, the number of matches and the time left in the timer
 function init() {
+	//initializing the array to hold the number of cards clicked on to an empty array
 	chosen = [];
+	//initializing the number of matches to 0
 	matches = 0;
+	//initializing the time left in the timer
 	timerLeft = 60;
 }
 
 //function to create the flipping action on cards or not based on whether or not they're a match
 function flipped(cardContainer) {
+	//variable to hold the value of the parameter
 	const card = cardContainer;
+	//if there are no cards chosen let the first card equal the parameter value
 	if (chosen.length === 0) {
 		card1 = card;
+	//else if there is one card chosen let the second card chosen equal the parameter value
 	} else if (chosen.length === 1) {
 		card2 = card;
 	}
+	//otherwise push the chosen cards id into the chosen array
 	chosen.push(card.dataset.id);
 
+	//if two cards have been chosen make a series of decisions based on whether or not those cards are matches
 	if (chosen.length === 2) {
+		//if the cards are a match, increment the matches variable, reset the chosen array, and check to see if that was the last matching pair
 		if (chosen[0] === chosen[1]) {
 			matches++;
 			chosen = [];
 			checkWin();
+		//otherwise reset the chosen array and flip the cards back over after 1 second
 		} else {
 			chosen = [];
 			const cardsToFlip = [card1, card2];
@@ -111,14 +129,21 @@ function flipped(cardContainer) {
 
 //function to check if a player has matched all cards and therefor won
 function checkWin() {
+	//variable to hold the value of the chosen board size
 	const boardSize = parseInt(cardNumberSelector.value);
-	console.log("boardSize:", boardSize);
+	//variable to hold the total number of matches needed to win
 	const totalPairs = boardSize / 2;
+	//if the number of matches made is the same as the number of matches needed to win go through winning logic
 	if (matches === totalPairs) {
+		//pause the timer at its current time
 		clearInterval(timer);
+		//display a message to let the player know that they won the game
 		statusMessage.textContent = "You won!";
+		//reenable the start button so the player can play again
 		startButton.style.pointerEvents = "auto";
+		//disable the ability to click on the board
 		gameBoard.style.pointerEvents = "none";
+		//reenable the ability to choose the board size
 		cardNumberSelector.style.pointerEvents = "auto";
 	}
 }
@@ -140,31 +165,42 @@ function checkLoss() {
 
 //function to update the timer as the seconds count down
 function updateTimer() {
+	//updating the timerLeft variable to decrement it by one as it counts down
 	timerLeft = timerLeft - 1;
+	//if the time is greater than or equal to 0 show the seconds remaining in the timer div
 	if (timerLeft >= 0) {
 		document.getElementById(
 			"timer"
 		).innerHTML = `${timerLeft} seconds remaining`;
 	}
+	//otherwise go through loss logic
 	checkLoss();
 }
 
 //function to reset the board
 function resetBoard() {
+	//initialize chosen to an empty array
 	chosen = [];
+	//initialize matches to 0
 	matches = 0;
+	//reset the timer
 	timerLeft = 60;
+	//clear the timer
 	clearInterval(timer);
+	//turn all the cards back over the the blank side
 	cards.forEach((card) => {
 		card.querySelector(".flip-card-inner").style.transform =
 			"rotateY(0deg)";
 	});
+	//empty out the win or loss message
 	statusMessage.textContent = "";
+	//hide the timer
 	timerDiv.style.display = "none";
 }
 
-//function to shuffle the board upon reload of page
+//function to shuffle the board
 (function shuffle() {
+	//for each of the cards randomize the position on the game board
 	cards.forEach((card) => {
 		let ramdomPos = Math.floor(Math.random() * 24);
 		card.style.order = ramdomPos;
